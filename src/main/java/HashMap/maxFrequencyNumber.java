9f259aab7f1a6b636c3efba45979dfdbf99a8b1f
rama sai <rama.sai@decimal.co.in>
2022-09-17 17:42:08 +0530

    Added maxFrequencyNumber code

--- src/main/java/HashMap/maxFrequencyNumber.java
+++ src/main/java/HashMap/maxFrequencyNumber.java
@@ -1,6 +1,8 @@
 package HashMap;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class maxFrequencyNumber {
@@ -16,7 +18,23 @@ public class maxFrequencyNumber {
     }
 
     public static int maxFrequencyNumber(Integer []arr){
-
-
+        Map<Integer, Integer > h = new HashMap<>();
+        for(Integer x :arr){
+            if(h.containsKey(x)){
+                h.put(x,h.get(x)+1);
+            }
+            else{
+                h.put(x,1);
+            }
+        }
+        int max =0;
+        int maxKey = Integer.MIN_VALUE;
+        for(int i =0; i<arr.length; i++){
+            if(h.get(arr[i])>max){
+                max = h.get(arr[i]);
+                maxKey = arr[i];
+            }
+        }
+        return maxKey;
     }
 }
